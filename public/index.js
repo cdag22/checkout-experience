c42f24046acfd02164e53256414d9af4208b882c
cj dagostino <cjd.dagostino@gmail.com>
2019-05-08 19:49:33 -0600

    Front end complete.

--- public/index.js
+++ public/index.js
@@ -1,6 +1,8 @@
-import App from './App.js';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from 'App';
 // const React = require('react');
-// const App = require('./components/App');
+// const App = require('./App');
 
-ReactDOM.render(React.createElement(App, null), document.getElementById('app'));
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL2NsaWVudC9pbmRleC5qcyJdLCJuYW1lcyI6WyJBcHAiLCJSZWFjdERPTSIsInJlbmRlciIsImRvY3VtZW50IiwiZ2V0RWxlbWVudEJ5SWQiXSwibWFwcGluZ3MiOiJBQUFBLE9BQU9BLEdBQVAsTUFBZ0IsVUFBaEI7QUFDQTtBQUNBOztBQUVBQyxTQUFTQyxNQUFULENBQWdCLG9CQUFDLEdBQUQsT0FBaEIsRUFBeUJDLFNBQVNDLGNBQVQsQ0FBd0IsS0FBeEIsQ0FBekIiLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgQXBwIGZyb20gJy4vQXBwLmpzJztcbi8vIGNvbnN0IFJlYWN0ID0gcmVxdWlyZSgncmVhY3QnKTtcbi8vIGNvbnN0IEFwcCA9IHJlcXVpcmUoJy4vY29tcG9uZW50cy9BcHAnKTtcblxuUmVhY3RET00ucmVuZGVyKDxBcHAgLz4sIGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdhcHAnKSk7Il19
+// ReactDOM.render(<App />, document.getElementById('app'));
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL2NsaWVudC9pbmRleC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBOztBQUVBIiwiZmlsZSI6ImluZGV4LmpzIiwic291cmNlc0NvbnRlbnQiOlsiLy8gaW1wb3J0IFJlYWN0IGZyb20gJ3JlYWN0Jztcbi8vIGltcG9ydCBSZWFjdERPTSBmcm9tICdyZWFjdC1kb20nO1xuLy8gaW1wb3J0IEFwcCBmcm9tICdBcHAnO1xuLy8gY29uc3QgUmVhY3QgPSByZXF1aXJlKCdyZWFjdCcpO1xuLy8gY29uc3QgQXBwID0gcmVxdWlyZSgnLi9BcHAnKTtcblxuLy8gUmVhY3RET00ucmVuZGVyKDxBcHAgLz4sIGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdhcHAnKSk7Il19
